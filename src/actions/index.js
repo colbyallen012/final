3e2ed6dc2ab40ec0078647d91c8254db682a6b72
Colby Allen <colbyallen012@gmail.com>
2019-07-31 09:55:24 -0600

    add is loading and has errored actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,13 @@ export const showAnimals = (animals) => ({
   type: 'SHOW_ANIMALS',
   animals
 })
+
+export const isLoading = (isLoading) => ({
+  type: 'IS_LOADING',
+  isLoading
+});
+
+export const hasErrored = (errorMsg) => ({
+  type: 'HAS_ERRORED',
+  errorMsg
+});
